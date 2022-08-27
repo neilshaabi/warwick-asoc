1e8584f56168b3c4017bbaf2ed9b6beb1a7e9fe0
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2022-08-27 17:53:42 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
     // Toggle active state for navbar link when selected
     var pathname = window.location.pathname;
-    var links = document.getElementsByTagName('a');
+    var links = document.getElementsByClassName('nav-link');
     for (var i = 0; i < links.length; i++) {
         if (pathname == links[i].getAttribute('href')) {
             links[i].classList.add('active');
