656a2b8592e4ea923f475d885a095d148964de24
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2022-09-13 23:58:29 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,22 +3,22 @@ from os import environ
 # Default config values
 class Config(object):
     TESTING = False
-    SECRET_KEY = environ["SECRET_KEY"], # Randomly generated with os.urandom(12).hex()
+    SECRET_KEY = environ["SECRET_KEY"] # Randomly generated with os.urandom(12).hex()
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask Mail setup
-    MAIL_SERVER = "smtppro.zoho.eu",
-    MAIL_PORT = 465,
-    MAIL_USE_SSL = True,
-    MAIL_USE_TLS = False,
-    MAIL_USERNAME = "no-reply@warwick-asoc.co.uk",
-    MAIL_PASSWORD = environ["MAIL_PASSWORD"],
-    MAIL_DEFAULT_SENDER = "no-reply@warwick-asoc.co.uk",
+    MAIL_SERVER = "smtppro.zoho.eu"
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USE_TLS = False
+    MAIL_USERNAME = "no-reply@warwick-asoc.co.uk"
+    MAIL_PASSWORD = environ["MAIL_PASSWORD"]
+    MAIL_DEFAULT_SENDER = "no-reply@warwick-asoc.co.uk"
     MAIL_SUPPRESS_SEND = False
     
     # Stripe Checkout setup
-    STRIPE_SECRET_KEY = environ["STRIPE_SECRET_KEY"],
-    STRIPE_PUBLISHABLE_KEY = environ["STRIPE_PUBLISHABLE_KEY"],
+    STRIPE_SECRET_KEY = environ["STRIPE_SECRET_KEY"]
+    STRIPE_PUBLISHABLE_KEY = environ["STRIPE_PUBLISHABLE_KEY"]
     STRIPE_ENDPOINT_SECRET = environ["STRIPE_ENDPOINT_SECRET"]
 
 
