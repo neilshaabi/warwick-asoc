63bde610a55c12d3abf53d6b6367e1de8f664c4f
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2022-10-05 14:41:09 +0100

    Create whatsapp_script.py

---
+++ whatsapp_script.py
@@ -0,0 +1,42 @@
+from os import system
+from datetime import date
+from pywhatkit import sendwhatmsg_to_group_instantly
+import pandas as pd
+
+today = date.today().strftime("%d-%m-%Y")
+filename = f"asoc_{today}.csv"
+
+# Retrieve data from heroku postgres db, write to csv file
+system(
+    f"heroku pg:psql -a warwick-asoc \
+        -c \"\copy ( \
+        SELECT first_name, last_name, membership, student_id, is_exec, verified  \
+        FROM users \
+        WHERE verified \
+        ORDER BY is_exec desc, membership, first_name \
+            ) \
+        TO '~/Desktop/{filename}' \
+        WITH CSV DELIMITER ',' HEADER;\""
+)
+
+# Process data into individual categories
+print("\nProcessing data...")
+df = pd.read_csv(filename)
+members = df.query("membership == membership")
+exec = df.query("is_exec == 't'")
+nonExec = df.query("is_exec == 'f'")
+execMembers = pd.merge(members, exec, how="inner")
+nonExecMembers = pd.merge(members, nonExec, how="inner")
+
+# Format statistics
+msg = f"""\n*ASOC User Stats ({today})*\n
+(Note: this is an automated message)
+Total accounts: {len(df)}
+Total members: {len(members)}
+Exec memberships: {len(execMembers)}/{len(exec)}
+Non-exec memberships: {len(nonExecMembers)}/{len(nonExec)}\n"""
+
+# Send stats to ASOC Tech group chat
+print("Sending message to ASOC Tech group chat...")
+sendwhatmsg_to_group_instantly("Btzj1dCFWKq2RvldJDMf5U", msg)
+print("Message sent!\n")
