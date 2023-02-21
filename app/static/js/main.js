0dd43e3513d8e29ee7522517d150abbc863d518f
Neil Shaabi <66903165+neilshaabi@users.noreply.github.com>
2023-02-21 16:56:34 +0000

    Update main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -60,11 +60,6 @@ $(document).ready(function() {
         slidesPerView: 2,
         loop: true,
 
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-
         pagination: {
             el: '.swiper-pagination',
             dynamicBullets: true,
